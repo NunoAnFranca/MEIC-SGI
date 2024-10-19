f09c4cecbe948412807a645170623fca73aa0c4f
Nuno <up201807530@fe.up.pt>
2024-10-19 09:40:35 +0100

    beetle

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { MyAxis } from './MyAxis.js';
+import { NURBSSurface } from 'three/addons/curves/NURBSSurface.js';
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+import { MyNurbsBuilder } from './MyNurbsBuilder.js';
 
 /**
  *  This class contains the contents of out application
@@ -451,60 +454,101 @@ class MyContents  {
     }
 
     buildBeetle(){
-        let beetle = new THREE.Group()
-        const materialBeetle = new THREE.LineBasicMaterial({color: "#ffffff"});
+        const materialBeetle = new THREE.LineBasicMaterial({color: "#000000", lineWidth: 100});
 
-        let points = [
-            new THREE.Vector3(-2,0,0),                                      // Left wheel & Left hull
-            new THREE.Vector3(-2,0.75*(4/3),0),                             // Left wheel
-            new THREE.Vector3(-0.5,0.75*(4/3),0),                           // Left wheel
-            new THREE.Vector3(-0.5,0,0),                                    // Left wheel
-            new THREE.Vector3(-2,2*(4/3)*(Math.sqrt(2)-1),0),               // Left hull
-            new THREE.Vector3(-2*(4/3)*(Math.sqrt(2)-1),2,0),               // Left hull
-            new THREE.Vector3(0,2,0),                                       // Left hull & Right top hull
-            new THREE.Vector3((4/3)*(Math.sqrt(2)-1),2,0),                  // Right top hull
-            new THREE.Vector3(1,1+(4/3)*(Math.sqrt(2)-1),0),                // Right top hull
-            new THREE.Vector3(1,1,0),                                       // Right top hull & Right bottom hull
-            new THREE.Vector3(1+(4/3)*(Math.sqrt(2)-1),1,0),                // Right bottom hull
-            new THREE.Vector3(2,(4/3)*(Math.sqrt(2)-1),0),                  // Right bottom hull
-            new THREE.Vector3(2,0,0),                                       // Right bottom hull & Right wheel
-            new THREE.Vector3(2,0.75*(4/3),0),                              // Right wheel
-            new THREE.Vector3(0.5,0.75*(4/3),0),                            // Right wheel
-            new THREE.Vector3(0.5,0,0)                                      // Right wheel
-        ]
+        for(let i = 0; i<30;i++){
 
-        //Left wheel
-        const curve1 = new THREE.CubicBezierCurve3(points[0], points[1], points[2], points[3]);
-        const geometry1 = new THREE.BufferGeometry().setFromPoints(curve1.getPoints(50));
-        const curve1Mesh = new THREE.Line(geometry1, materialBeetle);
-        beetle.add(curve1Mesh);
+            let beetle = new THREE.Group();
+            let points = [
+                new THREE.Vector3(-2,0,0),                                      // Left wheel & Left hull
+                new THREE.Vector3(-2,0.75*(4/3),0),                             // Left wheel
+                new THREE.Vector3(-0.5,0.75*(4/3),0),                           // Left wheel
+                new THREE.Vector3(-0.5,0,0),                                    // Left wheel
+                new THREE.Vector3(-2,2*(4/3)*(Math.sqrt(2)-1),0),               // Left hull
+                new THREE.Vector3(-2*(4/3)*(Math.sqrt(2)-1),2,0),               // Left hull
+                new THREE.Vector3(0,2,0),                                       // Left hull & Right top hull
+                new THREE.Vector3((4/3)*(Math.sqrt(2)-1),2,0),                  // Right top hull
+                new THREE.Vector3(1,1+(4/3)*(Math.sqrt(2)-1),0),                // Right top hull
+                new THREE.Vector3(1,1,0),                                       // Right top hull & Right bottom hull
+                new THREE.Vector3(1+(4/3)*(Math.sqrt(2)-1),1,0),                // Right bottom hull
+                new THREE.Vector3(2,(4/3)*(Math.sqrt(2)-1),0),                  // Right bottom hull
+                new THREE.Vector3(2,0,0),                                       // Right bottom hull & Right wheel
+                new THREE.Vector3(2,0.75*(4/3),0),                              // Right wheel
+                new THREE.Vector3(0.5,0.75*(4/3),0),                            // Right wheel
+                new THREE.Vector3(0.5,0,0)                                      // Right wheel
+            ]
+    
+            //Left wheel
+            const curve1 = new THREE.CubicBezierCurve3(points[0], points[1], points[2], points[3]);
+            const geometry1 = new THREE.BufferGeometry().setFromPoints(curve1.getPoints(50));
+            const curve1Mesh = new THREE.Line(geometry1, materialBeetle);
+            beetle.add(curve1Mesh);
+    
+            //Left hull
+            const curve2 = new THREE.CubicBezierCurve3(points[0],points[4], points[5], points[6]);
+            const geometry2 = new THREE.BufferGeometry().setFromPoints(curve2.getPoints(50));
+            const curve2Mesh = new THREE.Line(geometry2, materialBeetle);
+            beetle.add(curve2Mesh);
+    
+            // Right top hull
+            const curve3 = new THREE.CubicBezierCurve3(points[6],points[7], points[8], points[9]);
+            const geometry3 = new THREE.BufferGeometry().setFromPoints(curve3.getPoints(50)); 
+            const curve3Mesh = new THREE.Line(geometry3, materialBeetle);
+            beetle.add(curve3Mesh);
+    
+            // Right bottom hull
+            const curve4 = new THREE.CubicBezierCurve3(points[9],points[10], points[11], points[12]);
+            const geometry4 = new THREE.BufferGeometry().setFromPoints(curve4.getPoints(50)); 
+            const curve4Mesh = new THREE.Line(geometry4, materialBeetle);
+            beetle.add(curve4Mesh);
+    
+            // Right wheel
+            const curve5 = new THREE.CubicBezierCurve3(points[12],points[13], points[14], points[15]);
+            const geometry5 = new THREE.BufferGeometry().setFromPoints(curve5.getPoints(50)); 
+            const curve5Mesh = new THREE.Line(geometry5, materialBeetle);
+            beetle.add(curve5Mesh);
+    
+            beetle.scale.set(0.5,0.5,0.5);
+            beetle.position.set(5,5.5-i*0.002,this.roomWidth/2-0.05);
+            beetle.rotation.y = Math.PI;
+            this.app.scene.add(beetle);
 
-        //Left hull
-        const curve2 = new THREE.CubicBezierCurve3(points[0],points[4], points[5], points[6]);
-        const geometry2 = new THREE.BufferGeometry().setFromPoints(curve2.getPoints(50));
-        const curve2Mesh = new THREE.Line(geometry2, materialBeetle);
-        beetle.add(curve2Mesh);
+        }
+    }
 
-        // Right top hull
-        const curve3 = new THREE.CubicBezierCurve3(points[6],points[7], points[8], points[9]);
-        const geometry3 = new THREE.BufferGeometry().setFromPoints(curve3.getPoints(50)); 
-        const curve3Mesh = new THREE.Line(geometry3, materialBeetle);
-        beetle.add(curve3Mesh);
+    // Line2 for sprig
 
-        // Right bottom hull
-        const curve4 = new THREE.CubicBezierCurve3(points[9],points[10], points[11], points[12]);
-        const geometry4 = new THREE.BufferGeometry().setFromPoints(curve4.getPoints(50)); 
-        const curve4Mesh = new THREE.Line(geometry4, materialBeetle);
-        beetle.add(curve4Mesh);
+    // 
+    buildFlower(){
+        const materials = {
+            center: new THREE.LineBasicMaterial({color: "#4CF038"}),
+            petals: new THREE.LineBasicMaterial({color: "FF1D8D"})
+        }
+        
+        let flower = new THREE.Group();
+        
+        let pointsCircle = [
+            new THREE.Vector3(-1,0,0),
+            new THREE.Vector3(-1,(4/3),0),
+            new THREE.Vector3(1,(4/3),0),
+            new THREE.Vector3(1,0,0),
+            new THREE.Vector3(-1,-(4/3),0),
+            new THREE.Vector3(1, (4/3), 0)
+        ];
+        
+        const topCircle = new THREE.CubicBezierCurve3(pointsCircle[0], pointsCircle[1], pointsCircle[2], pointsCircle[3]);
+        const topCircleGeometry = new THREE.BufferGeometry().setFromPoints(topCircle.getPoints(50));
+        const topCircleMesh = new THREE.Line(topCircleGeometry, materials.center);
+        flower.add(topCircleMesh);
 
-        // Right wheel
-        const curve5 = new THREE.CubicBezierCurve3(points[12],points[13], points[14], points[15]);
-        const geometry5 = new THREE.BufferGeometry().setFromPoints(curve5.getPoints(50)); 
-        const curve5Mesh = new THREE.Line(geometry5, materialBeetle);
-        beetle.add(curve5Mesh);
+        const downCircle = new THREE.CubicBezierCurve3(pointsCircle[0], pointsCircle[4], pointsCircle[5], pointsCircle[3]);
+        const downCircleGeometry = new THREE.BufferGeometry().setFromPoints(downCircle.getPoints(50));
+        const downCircleMesh = new THREE.Line(downCircleGeometry, materials.center);
+        flower.add(downCircleMesh);
 
-        beetle.position.set(5,0,0);
-        this.app.scene.add(beetle);
+
+        flower.position.set(-5,5.5,this.roomWidth/2-0.05);
+        this.app.scene.add(flower);
     }
 
     /**
@@ -561,10 +605,13 @@ class MyContents  {
         this.buildCake();
         this.buildRadio();
         this.buildBeetle();
+        this.buildFlower();
         this.buildFrame(0.1, 2, 3, 0.1, 3.5, 6, false, 'textures/luis.jpg',"#ffffff", 'back');
         this.buildFrame(0.1, 2, 3, 0.1, -3.5, 6, false, 'textures/nuno.jpg',"#ffffff", 'back');
         this.buildFrame(0.1, 6, 3, 0.1, 5, 6, true, 'textures/landscape1.jpg',"#423721", 'left');
         this.buildFrame(0.1, 6, 3, 0.1, -5, 6, true, 'textures/landscape2.jpg', "#423721", 'left');
+        this.buildFrame(0.1, 3, 2, 0.1, 5, 6, false, 'textures/cork.jpg', "#ffffff", 'front');
+        this.buildFrame(0.1, 3, 2, 0.1, -5, 6, false, 'textures/cork.jpg', "#ffffff", 'front')
         this.buildLamp();
     }
     
