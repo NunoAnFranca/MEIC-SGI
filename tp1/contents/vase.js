1202d818d62e39679e0364d287c51c21f60b0e5a
Nuno <up201807530@fe.up.pt>
2024-10-25 15:53:05 +0100

    Vase done

--- tp1/contents/vase.js
+++ tp1/contents/vase.js
@@ -17,13 +17,17 @@ class MyVase  {
         this.samplesV = 32;
 
         const textures = {
-            vase1 : this.loader.load('textures/vase1.jpg')
+            vase1 : this.loader.load('textures/vase1.jpg'),
+            clay : this.loader.load('textures/clay.jpg')
         }
 
         textures.vase1.colorSpace = THREE.SRGBColorSpace;
+        textures.clay.colorSpace = THREE.SRGBColorSpace;
+
 
         this.materials = {
-            vase1: new THREE.MeshPhongMaterial({color: "#ffffff", specular: "#ffffff", map: textures.vase1})
+            vase1: new THREE.MeshPhongMaterial({color: "#ffffff", specular: "#ffffff", map: textures.vase1}),
+            clay:  new THREE.MeshPhongMaterial({color: "#ffffff", specular: "#111111", map: textures.clay}),
         }
     }
 
@@ -33,47 +37,72 @@ class MyVase  {
     buildVase(positionX, positionZ){
         this.materials.vase1.side = THREE.DoubleSide;
         
+        let radialSegments = 32;
+        let plateHeight = 0.2;
+        let platewidth = 2;
         let vase = new THREE.Group();
         let controlPoints;
         let surfaceData;
         let mesh;
         let orderU = 3;
-        let orderV = 2;
+        let orderV = 4;
 
         controlPoints = [
             // U = 0
-            [ // V = 0..1
-                [-2.0, -3.0, 0.0, 1],
-                [-3.0, 0.0, 0.0, 1],
-                [-1.0, 3.0, 0.0, 1]
+            [ // V = 0..4
+                [-1,0,0,1],
+                [-3,1,0,1],
+                [-1/2,5,0,1],
+                [-1.5,6,0,1],
+                [-2,8,0,1]
+
             ],
             // U = 1
-            [ // V = 0..1
-                [0, -3.0,-2.0, 1],
-                [0, 0,-3.0, 1],
-                [0, 3.0, -1.0, 1]
+            [ // V = 0..4
+                [-1,0,-4/3,1],
+                [-3,1,-4,1],
+                [-1/2,5,-2/3,1],
+                [-1.5,6,-2,1],
+                [-2,8,-8/3,1]
+
             ],
             // U = 2
-            [ // V = 0..1
-                [2.0, -3.0, 0, 1],
-                [3.0, 0, 0, 1],
-                [1.0, 3.0, 0, 1]
+            [ // V = 0..4
+                [1,0,-4/3,1],
+                [3,1,-4,1],
+                [1/2,5,-2/3,1],
+                [1.5,6,-2,1],
+                [2,8,-8/3,1]
             ],
             // U = 3
-            [ // V = 0..1
-                [0, -3.0, 2.0, 1],
-                [0, 0, 3.0, 1],
-                [0, 3.0, 1.0, 1]
+            [ // V = 0..4
+                [1,0,0,1],
+                [3,1,0,1],
+                [1/2,5,0,1],
+                [1.5,6,0,1],
+                [2,8,0,1]
             ]
         ];
 
         surfaceData = this.builder.build(controlPoints, orderU, orderV, this.samplesU, this.samplesV, this.materials.vase1);
         mesh = new THREE.Mesh(surfaceData, this.materials.vase1);
+        mesh.position.set(0,plateHeight/2,0);
         vase.add(mesh);
         this.meshes.push(mesh);
 
+        surfaceData = this.builder.build(controlPoints, orderU, orderV, this.samplesU, this.samplesV, this.materials.vase1);
+        mesh = new THREE.Mesh(surfaceData, this.materials.vase1);
+        mesh.rotation.y = Math.PI;
+        mesh.position.set(0,plateHeight/2,0);
+        vase.add(mesh);
+        this.meshes.push(mesh);
 
-        vase.position.set(positionX, 5,positionZ);
+        const plate = new THREE.CylinderGeometry(platewidth, platewidth, plateHeight, radialSegments, radialSegments);
+        const plateMesh = new THREE.Mesh(plate, this.materials.clay);
+        plateMesh.position.set(0,plateHeight/2,0);
+        vase.add(plateMesh);
+
+        vase.position.set(positionX, 0,positionZ);
         vase.scale.set(0.25,0.25,0.25);
         this.app.scene.add(vase);
     }
