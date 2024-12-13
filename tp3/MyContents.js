0c4189d03cd2e599576dd64bfa78c96d5193c8b0
Nuno <up201807530@fe.up.pt>
2024-12-13 15:55:22 +0000

    Change balloon position

--- tp3/MyContents.js
+++ tp3/MyContents.js
@@ -14,20 +14,6 @@ class MyContents {
         this.app = app;
         this.axis = null;
 
-        // box related attributes
-        this.boxMesh = null;
-
-        // plane related attributes
-        this.diffusePlaneColor = "#00ffff";
-        this.specularPlaneColor = "#777777";
-        this.planeShininess = 30;
-        this.planeMaterial = new THREE.MeshPhongMaterial({
-            color: this.diffusePlaneColor,
-            specular: this.specularPlaneColor,
-            emissive: "#000000",
-            shininess: this.planeShininess,
-        });
-
         //picking: read documentation of THREE.Raycaster
 
         this.raycaster = new THREE.Raycaster()
@@ -48,7 +34,7 @@ class MyContents {
         // NOTICE: not a ThreeJS facility
         // this.notPickableObjIds = []
         // this.notPickableObjIds = ["col_0_0", "col_2_0", "col_1_1"]
-        this.notPickableObjIds = ["myplane"]
+        this.notPickableObjIds = ["A", "B"]
       
         //register events
 
@@ -88,8 +74,6 @@ class MyContents {
         this.initialPoistions = ["A","B"];
         this.buildInitialPosition("A",3,2);
         this.buildInitialPosition("B",3,-2);
-        // Create a Plane Mesh with basic material
-        this.buildPlane()
 
     }
 
@@ -113,21 +97,6 @@ class MyContents {
         const ambientLight = new THREE.AmbientLight(0x555555)
         this.app.scene.add(ambientLight)
     }
-
-    /*
-    *
-    * Setup plane
-    *
-    */
-    buildPlane() {
-        let plane = new THREE.PlaneGeometry(10, 10);
-        this.planeMesh = new THREE.Mesh(plane, this.planeMaterial);
-        this.planeMesh.name = "myplane"
-        this.planeMesh.rotation.x = -Math.PI / 2;
-        this.planeMesh.position.y = 0;
-        this.app.scene.add(this.planeMesh);   // plane is not in any layer
-    }
-
 
     buildBaloonsColumn(name, color, posz) {
         for (let i = 0; i < 3; i++) {
@@ -231,14 +200,13 @@ class MyContents {
         this.lastPickedObj = null;
     }
 
-    changeObjectPosition(lastObj, obj) {
+    changeObjectPosition( obj) {
         if (this.lastPickedObj != obj) {
             if (this.lastPickedObj){
                 this.lastPickedObj.position.set(obj.position.x,obj.position.y+0.8,obj.position.z);
             }
-            this.lastPickedObj = obj;
-            this.lastPickedObj.currentHex = this.lastPickedObj.material.color.getHex();
-            this.lastPickedObj.material.color.setHex(this.pickingColor);
+            this.lastPickedObj.material.color.setHex(this.lastPickedObj.currentHex);
+            this.lastPickedObj = null;
         }
     }
 
@@ -250,13 +218,16 @@ class MyContents {
         if (intersects.length > 0) {
             const obj = intersects[0].object
             if (this.notPickableObjIds.includes(obj.name)) {
-                this.restoreColorOfFirstPickedObj()
-                console.log("Object cannot be picked !")
-            }
-            else if(this.initialPoistions.includes(obj.name)){
-                console.log("Object to choose");
-                console.log(this.lastPickedObj);
-                this.changeObjectPosition(this.lastPickedObj, obj);
+                if(this.lastPickedObj){
+                    console.log("Object to choose");
+                    if(!this.initialPoistions.includes(this.lastPickedObj.name)){
+                        this.changeObjectPosition(obj)
+                    }
+                }
+                else{
+                    this.restoreColorOfFirstPickedObj()
+                    console.log("Object cannot be picked !")
+                }
             }
             else
                 this.changeColorOfFirstPickedObj(obj)
