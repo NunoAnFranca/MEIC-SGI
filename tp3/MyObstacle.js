bdcd537ee52284c7ddd85864004546a39e5ebe2e
Luís Alves <luiscnalves@gmail.com>
2025-01-02 12:44:59 +0000

    added obstacles bounding box

--- tp3/MyObstacle.js
+++ tp3/MyObstacle.js
@@ -23,14 +23,14 @@ class MyObstacle {
     const boundingBox = new THREE.Box3().setFromObject(mesh);
     const helper = new THREE.Box3Helper(boundingBox, 0xffff00);
     
-    const boundingSphere = boundingBox.getBoundingSphere(new THREE.Sphere());
-    const sphereGeometry = new THREE.SphereGeometry(boundingSphere.radius, 32, 32);
-    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: 0.5 });
-    const sphereHelper = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    //const boundingSphere = boundingBox.getBoundingSphere(new THREE.Sphere());
+    //const sphereGeometry = new THREE.SphereGeometry(boundingSphere.radius, 32, 32);
+    //const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: 0.5 });
+    //const sphereHelper = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-    sphereHelper.position.copy(boundingSphere.center);
-    
-    this.app.scene.add(sphereHelper);
+    //sphereHelper.position.copy(boundingSphere.center);
+
+    //this.app.scene.add(sphereHelper);
     this.app.scene.add(helper);
     this.app.scene.add(mesh);
   }
